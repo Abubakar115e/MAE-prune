61d89a95226d93d29488afa6d04605fb0816152b
Abubakar115e <113997856+Abubakar115e@users.noreply.github.com>
2024-03-17 18:29:57 +0100

    Delete sbatchScript.sh

--- sbatchScript.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=VitJob
-#SBATCH --ntasks=1 --cpus-per-task=10 --mem=20000M
-#SBATCH --time=24:00:00
-#SBATCH -p gpu --gres=gpu:A100:1
-module load python/3.9.9
-python main.py --eval --load_compression_rate --data-path $path_to_imagenet$ --model vit_huge_patch14_mae --target_flops 103.4
