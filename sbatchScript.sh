27af195307e174b77a090fcf8ac129b6a4691402
Abubakar115e <113997856+Abubakar115e@users.noreply.github.com>
2024-03-18 00:00:03 +0100

    Update sbatchScript.sh

--- sbatchScript.sh
+++ sbatchScript.sh
@@ -3,12 +3,7 @@
 #SBATCH --ntasks=1 --cpus-per-task=10 --mem=40000M
 #SBATCH --time=24:00:00
 #SBATCH -p gpu --gres=gpu:titanrtx:1
-
-# Set the path to the ImageNet dataset
-export path_to_imagenet=/MAE-prune
-
-# Load Python module
+mkdir -p /MAE-prune/train
+export path_to_imagenet = /MAE-prune/train
 module load python/3.9.9
-
-# Run the Python script with the environment variable
-python main.py --eval --load_compression_rate --data-path $path_to_imagenet --model vit_huge_patch14_mae --target_flops 103.4
+python main.py --eval --load_compression_rate --data-path $path_to_imagenet$ --model vit_huge_patch14_mae --target_flops 103.4
