56e43898e5760bbb999cbeaa1e058a3ad644e2d2
Abubakar115e <113997856+Abubakar115e@users.noreply.github.com>
2024-03-18 17:30:07 +0100

    Update __init__.py

--- DiffRate/patch/__init__.py
+++ DiffRate/patch/__init__.py
@@ -7,6 +7,4 @@
 
 from .deit import apply_patch as deit
 from .mae import apply_patch as mae
-from .caformer import apply_patch as caformer
-
-__all__ = ["deit", "mae", "caformer"]
+__all__ = [ "mae", "deit"]
