d1a4cf56a8b0b4f218b7969d9181485a9bb4c759
Abubakar115e <113997856+Abubakar115e@users.noreply.github.com>
2024-03-18 14:46:16 +0100

    Update models_mae.py

--- models_mae.py
+++ models_mae.py
@@ -26,7 +26,7 @@ from timm.data import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
 def _cfg(url='', **kwargs):
     return {
         'url': url,
-        'num_classes': 1000, 'input_size': (3, 224, 224), 'pool_size': None,
+        'num_classes': 100, 'input_size': (3, 224, 224), 'pool_size': None,
         'crop_pct': .9, 'interpolation': 'bicubic',
         'mean': IMAGENET_DEFAULT_MEAN, 'std': IMAGENET_DEFAULT_STD,
         'first_conv': 'patch_embed.proj', 'classifier': 'head',
