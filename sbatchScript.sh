d2dd1bd40e4bc2e3eabca97521fae991f04bde04
Abubakar115e <113997856+Abubakar115e@users.noreply.github.com>
2024-03-18 16:50:05 +0100

    Update sbatchScript.sh

--- sbatchScript.sh
+++ sbatchScript.sh
@@ -2,6 +2,6 @@
 #SBATCH --job-name=VitJob
 #SBATCH --ntasks=1 --cpus-per-task=10 --mem=40000M
 #SBATCH --time=24:00:00
-#SBATCH -p gpu --gres=gpu:titanrtx:1
+#SBATCH -p gpu --gres=gpu:titanv:1
 module load python/3.9.9
 python main.py --eval --load_compression_rate --data-path $path_to_imagenet$ --model vit_huge_patch14_mae --target_flops 103.4
