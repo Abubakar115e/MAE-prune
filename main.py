4ed1cf254d0e95512c0381e46be67ad0d8d90336
Abubakar115e <113997856+Abubakar115e@users.noreply.github.com>
2024-03-19 18:02:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -172,7 +172,7 @@ def get_args_parser():
     # distributed training parameters
     parser.add_argument('--world_size', default=1, type=int,
                         help='number of distributed processes')
-    parser.add_argument('--port', default="15662", type=str,
+    parser.add_argument('--port', default="4331", type=str,
                         help='number of distributed processes')
     parser.add_argument('--dist_url', default='env://', help='url used to set up distributed training')
 
